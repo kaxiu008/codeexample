28ea0033c0cf1026c505325352fd54aac9c2941c
kaxiu008 <kaxiu008@gmail.com>
2021-08-29 16:08:34 +1200

    Update single.responsibility.principles.example.java

--- single.responsibility.principles.example.java
+++ single.responsibility.principles.example.java
@@ -10,16 +10,6 @@ public class HelloWorld{
         public Square(Integer length)
         {
             this.length = length;
-            System.out.println("length of a new Square is: " + this.length);
-        }
-        
-        public Integer getLength()
-        {
-            return length;
-        }
-        
-        public void test(){
-            System.out.println("This is a test");
         }
     }
     
@@ -52,36 +42,19 @@ public class HelloWorld{
             
             for(Object shape : this.shapes){
                 if(shape instanceof Square) {
-                    System.out.println("1");
-                    //System.out.println(shape);
-                    //System.out.println(shape.length);
-                    //System.out.println((Square)shape.length);
                     Square s = (Square)shape;
-                    System.out.println(s);
-                    System.out.println(s.length);
                     areas.add(Math.pow(s.length, 2));
                 }  else if (shape instanceof Circle) {
-                    System.out.println("2");
-                    //System.out.println(shape);
                     Circle c = (Circle)shape;
-                    System.out.println(c);
-                    System.out.println(c.radius);
                     areas.add(Math.PI*Math.pow(c.radius, 2));
                 }
             }
-            
-         /*   Double sum = 0.0;
-            for(Double d : areas)
-                sum += d;
-            System.out.println("this is the sum(): " + sum.toString()); 
-            return sum.toString(); */
             
             Double sum = 0.0;
             sum = areas.stream()
             .mapToDouble(a -> a)
             .sum();
 
-            //System.out.println(sum); 
             return sum.toString(); 
 
         }
@@ -99,23 +72,8 @@ public class HelloWorld{
             new Square(6),
         };
         
-        Square s2 = new Square(7);
-        Circle c1 = new Circle(4);
-        Square s1 = new Square(3);
-        System.out.println(s1);
-        System.out.println(s1.length);
-        System.out.println(s1.getLength());
-        
-        Object shapes1[] = new Object[]{c1, s1, s2,};
-        
         Caculator areas = new Caculator(shapes);
-        
-        //Date now = new Date();
-        //System.out.println("now: " + now);
-        //System.out.println("Hello World");
-        //System.out.println(shapes[1].test());
-        System.out.println("this is new Caculator object: " + areas);
-        System.out.println("this is the sum(): " + areas.sum());
+
         System.out.println("this is the output(): " + areas.output());
     }
     
